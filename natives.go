8cc979c8a3d802aae2096f57b0da1a43ac52d577
onikiychuk <Xeror19234>
2023-11-06 18:43:30 +0300

    CreateDynamicSphere

--- natives.go
+++ natives.go
@@ -2341,6 +2341,20 @@ func CreateDynamicMapIcon(x float32, y float32, z float32, iconType int, color i
 	))
 }
 
+// For documentation, please visit https://github.com/samp-incognito/samp-streamer-plugin/wiki/Natives
+func CreateDynamicSphere(x float32, y float32, z float32, size float32, worldId int, interiorId int, playerId int, priority int) int {
+	return int(C.CreateDynamicSphere(
+		C.float(x),
+		C.float(y),
+		C.float(z),
+		C.size(z),
+		C.int(worldId),
+		C.int(interiorId),
+		C.int(playerId),
+		C.int(priority),
+	))
+}
+
 // For documentation, please visit https://open.mp/docs/scripting/functions/AttachObjectToVehicle
 func AttachObjectToVehicle(objectid, vehicleid int, fOffsetX, fOffsetY, fOffsetZ, fRotX, fRotY, fRotZ float32) bool {
 	return bool(C.AttachObjectToVehicle(C.int(objectid), C.int(vehicleid), C.float(fOffsetX), C.float(fOffsetY), C.float(fOffsetZ), C.float(fRotX), C.float(fRotY), C.float(fRotZ)))
